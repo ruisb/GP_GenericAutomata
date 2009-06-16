f07e9b9df4ee9f3a70f48953c6fa6ce95bcc32c5
Rui Soares Barbosa <rui.soares.barbosa@gmail.com>
2009-06-16 17:16:33 +0200

    new rep

--- Examples.hs
+++ Examples.hs
@@ -49,13 +49,48 @@ prodLFA :: LFA a s1 -> LFA a s2 -> LFA a (s1,s2)
 prodLFA = undefined
 
 --bissimilarity
-bisimilarDFA :: DFA a s -> s -> s -> Bool
-bisimilarDFA = undefined
-bisimilarNFA :: NFA a s -> s -> s -> Bool
-bisimilarNFA d = undefined
+aut |> a = fromJust (M.lookup a aut)
 
-bisimilarLFA :: LFA a s -> s -> s -> Bool
-bisimilarLFA d = undefined
+bisimilarDFA :: (Eq s) => DFA a s -> s -> s -> Bool
+bisimilarDFA d p q = bisimilarDFA' d p q []
+
+bisimilarDFA' d p q visited 
+     | (p,q) `elem` visited = True
+     | (q,p) `elem` visited = True
+     | otherwise = let al = M.keys d
+                   in all[bisimilarDFA' d p' q' ((p,q):visited) | a <- al sp'<- (d |> a) p, q' <- (d |> a) q]
+--wrong
+
+--problem here : need to check if already visited to stop recursion.
+--dont know if there is a better way.
+                        
+
+bisimilarNFA :: (Ord s) => NFA a s -> s -> s -> Bool
+bisimilarNFA d p q = bisimilarNFA' d p q []
+
+bisimilarNFA' d p q visited
+    | (p,q) `elem` visited = True
+    | (q,p) `elem` visited = True
+    | otherwise  = let al = M.keys d
+                       ps = (d |> d) p
+                       qs = (d |> d) q 
+                   in all (\p'->any (\q'->bisimilarNFA' d p' q' ((p,q):visited)) qs) ps 
+                      &&
+                      all (\q'->any (\p'->bisimilarNFA' d p' q' ((p,q):visited)) ps) qs
+
+bisimilarLFA :: (Eq s) => LFA a s -> s -> s -> Bool
+bisimilarLFA d p q = bisimilarLFA' d p q []
+
+bisimilarLFA' d p q visited 
+    | (p,q) `elem` visited = True
+    | (q,p) `elem` visited = True
+    | otherwise  = let al = M.keys d
+                       ps = d |> d p
+                       qs = d |> d q 
+                   in setall (\p'->setany (\q'->bisimilarNFA' d p' q' ((p,q):visited)) qs) ps 
+                      &&
+                      setall (\q'->setany (\p'->bisimilarNFA' d p' q' ((p,q):visited)) ps) qs
 
 
-
+setall p = S.fold (&&) True  . S.map p
+setany p = S.fold (||) False . S.map p
