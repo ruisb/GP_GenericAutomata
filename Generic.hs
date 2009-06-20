031b15a8a687ea6e9dddfcf137261c888014e018
Rui Soares Barbosa <rui.soares.barbosa@gmail.com>
2009-06-21 01:21:37 +0200

    some corrections, finite functions utilities and tried product (not doable?)

--- Generic.hs
+++ Generic.hs
@@ -4,6 +4,8 @@ import Regular2.Base
 
 import qualified Data.Map as M
 import qualified Data.Set as S
+import Control.Monad
+import Control.Arrow
 
 -- Transition system
 type TS f s = s -> f s
@@ -39,4 +41,20 @@ run3 lts xs s = case xs of
   []   -> return s
   y:ys -> case M.lookup y lts of
     Nothing -> fail "Incomplete automaton!"
-    Just ts -> ts s >>= \s' -> run3 lts ys s'
+    Just ts -> ts s >>= run3 lts ys
+
+-- I now think product canot be defined so generically...
+--prod :: (Functor f) => LTS a f s1 -> LTS a f s2 -> LTS a f (s1,s2)
+--prod t1 t2 = let alph = M.keys t1
+--             in  M.fromList . map (\a ->(a, t1 \$ a *** t2 \$ a)) $ alph
+--
+
+--these are utility functions  to deal with finite function. 
+-- we do not want to always care about them being a map.
+-- todo: think how they could be done well
+infixl 9 \$
+m \$ k = guardFromJust "action not defined" (M.lookup k m)
+
+guardFromJust _   (Just x) = x
+guardFromJust err Nothing  = error err
+
